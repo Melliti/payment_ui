e10a9f699afbb9f2f61882b7b65c2866c8f591d1
Melliti <r.melliti@protonmail.com>
2020-05-19 15:22:53 -0400

    [del] useless code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="main">
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Todos from "../components/Todos"
-import AddTodo from "../components/AddTodo"
-import axios from 'axios'
+// import axios from 'axios'
 export default {
   name: 'Home',
   components: {
-    AddTodo,
-    Todos
   },
   data() {
     return {
@@ -23,25 +17,6 @@ export default {
     }
   },
   methods: {
-    deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(this.todos = this.todos.filter(todo => todo.id != id))
-      .catch(err => console.log(err))
-      },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-        })
-        .then(res => this.todos = [...this.todos, res.data])
-        .catch(err => console.log(err)        )
-      }
-    },
-  created() {
-    axios.get ('https://jsonplaceholder.typicode.com/todos?_limit=10')
-    .then(res => this.todos = res.data)
-    .catch(err => console.log(err))
   }
 }
 </script>
